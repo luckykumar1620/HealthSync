a94160aec859f606520a9fc36feaa82531329a92
Lucky Kumar <lucky9110132241@gmail.com>
2025-10-03 05:16:33 +0530

    add admin url in frontend

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -37,7 +37,7 @@ const Navbar = () => {
             <li className='py-1'>CONTACT</li>
             <hr className='border-none outline-none h-0.5 bg-blue-300 w-3/5 m-auto hidden' />
         </NavLink >
-        <a target="_blank" href="http://localhost:5174" className="border px-5 text-xs py-1.5 rounded-full font-medium">Admin Panel</a>
+        <a target="_blank" href="https://healthsync-admin.onrender.com" className="border px-5 text-xs py-1.5 rounded-full font-medium">Admin Panel</a>
       </ul>
       <div className='flex item-center gap-4'>
        
