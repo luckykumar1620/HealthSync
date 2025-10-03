49c367599747015cc40c5feec4c14876269065ab
Lucky Kumar <lucky9110132241@gmail.com>
2025-10-03 21:21:02 +0530

    fixes some issues

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -62,7 +62,8 @@ const Navbar = () => {
        {/* ---------mobile menu------------- */}
       <div className={`${showMenu ?'fixed w-full':'h-0 w-0'} md:hidden right-0 top-0 bottom-0 z-20 overflow-hidden bg-white transition-all`}>
         <div className='flex items-center justify-between px-5 py-6'>
-          <img className='w-36' src={assets.logo} alt="" />
+          {/* <img className='w-36' src={assets.logo} alt="" /> */}
+          <p className='font-bold text-4xl text-blue-500 '>HealthSync</p>
           <img className='w-7' onClick={()=>setShowmenu(false)} src={assets.cross_icon} alt="" />
         </div>
         <ul className='flex flex-col items-center gap-2 mt-5 px-5 text-lg font-medium '>
